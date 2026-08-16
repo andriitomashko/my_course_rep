e6c108ee701c8b7f4b7eeec4ed0a9481778599f0
Andrii T <tomashkoandrii@gmail.com>
2026-08-16 12:04:40 +0300

    Add CI skip for login test

--- tests/module10.spec.ts
+++ tests/module10.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('Verify login with valid credentials', async ({ page }) => {
+    test.skip(!!process.env.CI, 'Skip on CI');
+    
     await page.goto('/auth/login')
     await page.getByTestId('email').fill('customer@practicesoftwaretesting.com');
     await page.getByTestId('password').fill('welcome01');
