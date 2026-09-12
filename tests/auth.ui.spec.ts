f13ebc50e17f85517749f576190e5625a71cf016
Andrii T <tomashkoandrii@gmail.com>
2026-09-12 17:06:44 +0300

    fix: uncomment test.skip for CI environment in UI login test

--- tests/auth.ui.spec.ts
+++ tests/auth.ui.spec.ts
@@ -11,7 +11,7 @@ test('User can log in via UI', async ({ page }) => {
     const loginPage = new LoginPage(page);
     const accountPage = new AccountPage(page);
     const header = new HeaderFragment(page);
-    // test.skip(!!process.env.CI, 'Skip on CI');
+    test.skip(!!process.env.CI, 'Skip on CI');
 
     await loginPage.goto();
     await loginPage.performLogin(testUser.email, testUser.password);
